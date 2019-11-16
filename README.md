8525850c689663ecc326687cc1aa74cb3ae75998
fozzie77 <54290959+fozzie77@users.noreply.github.com>
2019-11-16 14:41:25 -0700

    commented out code

---
+++ README.md
@@ -0,0 +1,18 @@
+# Train Scheduler
+
+A simple site that allows you to input train schedules and saves that data for future use.
+
+# How To Use:
+
+Fill out the appropriate form boxes completely then click "submit". This will populate your train into the current schedule above and derive the next time the train will arrive from information given.
+
+# Demo:
+
+Check it out here: https://fozzie77.github.io/Train-Scheduler/
+
+# Build Tools:
+
+* Bootstrap
+* JQuery
+* Firebase
+* Moment.js
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,44 +1,46 @@
+//Train Scheduler
+$(document).ready(function () {
 
-$(document).ready (function(){
- 
+    //Global Variables
     var name;
     var destination;
     var firstTrain;
     var frequency = 0;
 
-database.ref ().on ("child_added", function (childInfo){
-   
-        var firstTrainNew = moment (childInfo.val ().firstTrain, "hh:mm");
-        var timeDifference = moment ().diff (moment(firstTrainNew), "minutes");
-        var minutesAway = childInfo.val ().frequency - timeDifference % childInfo.val ().frequency;
-        var nextArrival = moment ().add (minutesAway, "minutes").format ("hh:mm");
+    //Triggers when new train is pushed to firebase
+    database.ref().on("child_added", function (childInfo) {
 
-        console.log(firstTrainNew);
-        console.log(timeDifference);
-        console.log(minutesAway);
-        console.log(nextArrival);
+        var firstTrainNew = moment(childInfo.val().firstTrain, "hh:mm");
+        var timeDifference = moment().diff(moment(firstTrainNew), "minutes");
+        var minutesAway = childInfo.val().frequency - timeDifference % childInfo.val().frequency;
+        var nextArrival = moment().add(minutesAway, "minutes").format("hh:mm");
 
-        $("tbody").prepend ("<tr><td>" + childInfo.val ().name +
-                "</td><td>" + childInfo.val ().destination +
-                "</td><td>" + childInfo.val ().frequency +
-                "</td><td>" + nextArrival + 
-                "</td><td>" + minutesAway + "</td></tr>");
+        //puts train at top of list
+        $("tbody").prepend("<tr><td>" + childInfo.val().name +
+            "</td><td>" + childInfo.val().destination +
+            "</td><td>" + childInfo.val().frequency +
+            "</td><td>" + nextArrival +
+            "</td><td>" + minutesAway + "</td></tr>");
     });
 
-        $("#addTrainBtn").on ("click", function () {
-        event.preventDefault ();
-        name = $("#trainNameInput").val ().trim ();
-        destination = $("#destinationInput").val ().trim ();
-        firstTrain = $("#firstTrainInput").val ().trim ();
-        frequency = $("#frequencyInput").val ().trim ();
+    //Add train function
+    $("#addTrainBtn").on("click", function () {
+        //captures form input
+        event.preventDefault();
+        name = $("#trainNameInput").val().trim();
+        destination = $("#destinationInput").val().trim();
+        firstTrain = $("#firstTrainInput").val().trim();
+        frequency = $("#frequencyInput").val().trim();
 
-        database.ref ().push ({
+        //pushes to firebase
+        database.ref().push({
             name: name,
             destination: destination,
             firstTrain: firstTrain,
             frequency: frequency,
             dateAdded: firebase.database.ServerValue.TIMESTAMP
         });
-        $("form")[0].reset ();
+        //resets form for next train
+        $("form")[0].reset();
     });
 });
